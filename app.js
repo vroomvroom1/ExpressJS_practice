343c9b93d02e08e2113467a3c7a30b9cdfa48bc9
Matt <mattvroom1@gmail.com>
2018-04-06 20:23:29 -0400

    Connected to MongoDB

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-let express = require('express');
-let bodyParser = require('body-parser');
-let path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const expressValidator = require('express-Validator');
+const mongojs = require('mongojs');
+const db = mongojs('customerapp', ['users']);
+let ObjectId = mongojs.ObjectId;
 
-let app = express();
+
+const app = express();
 
 // let logger = function(req, res, next){
 //   console.log('Logging...');
@@ -11,7 +16,11 @@ let app = express();
 //
 // app.use(logger);
 
-//Body Parser
+// View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//Body Parser Middleware
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -19,8 +28,64 @@ app.use(bodyParser.urlencoded({extended: false}));
 // Set Static Path
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Global vars
+app.use(function(req, res, next){
+  res.locals.errors = null;
+  next();
+});
+
+
+// Express Validator Middleware
+app.use(expressValidator());
+
+
+
 app.get('/', function(req, res){
-  res.send('hello world');
+  db.users.find(function (err, docs) {
+  res.render('index', {
+    title: 'Customers',
+    users: docs
+  });
+})
+
+});
+
+app.post('/users/add', function(req, res){
+
+  req.checkBody('first_name', 'First Name is Required').notEmpty();
+  req.checkBody('last_name', 'Last Name is Required').notEmpty();
+
+  let errors = req.validationErrors();
+
+  if(errors){
+    res.render('index', {
+      title: 'Customers',
+      users: users,
+      errors: errors
+    });
+  } else {
+    let newUser = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name
+    }
+
+    db.users.insert(newUser, function(err, result){
+      if(err){
+        console.log(err);
+      }
+      res.redirect('/');
+    });
+  }
+
+});
+
+app.delete('/users/delete/:id', function(req, res){
+  db.users.remove({_id: ObjectId(req.params.id)}, function(err, result){
+    if(err){
+      console.log(err);
+    }
+    res.redirect('/');
+  });
 });
 
 app.listen(3000, function(){
